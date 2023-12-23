ef0d0109f5829573cc92c315649f528d2061d01b
Bheb Developer <bhebdeveloper@gmail.com>
2023-12-23 19:43:58 +0530

    Second Commit (Google Sheet)

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-// Developer: Ajay Singh
-// Version: 1st version
+// Author: Ajay Singh
+// Version: 1.0
 // Date: 2023-11-09
 
 async function loadCommands() {
-    // Fetching data from 'commands.csv'
-    const response = await fetch('https://gist.githubusercontent.com/ajayparihar/ea424724d874d67e2c5b52bf38d931f3/raw/b60ab4bb8b2bd75eaab87b9b4bcee4a5c746e48a/commands.csv');
+    // Fetching data from 'commands.csv' Github Gist (Raw)
+    // const response = await fetch('https://gist.githubusercontent.com/ajayparihar/ea424724d874d67e2c5b52bf38d931f3/raw/b60ab4bb8b2bd75eaab87b9b4bcee4a5c746e48a/commands.csv');
+    // Google sheet (publish as csv)
+    const response = await fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vTpgO5dkZtima-Pn9QPveTMsANWp-oMYBwNAc2xU0n-MsMiJKMSFqUP42xWOBZYQiUAoQsbnIysArka/pub?output=csv');
     const dataBuffer = await response.arrayBuffer();
     const workbook = XLSX.read(dataBuffer, { type: 'array' });
 
